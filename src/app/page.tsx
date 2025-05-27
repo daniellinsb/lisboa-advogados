3f9b5eea3830b2b392ed8bb9a7187faaf1411cd9
daniellinsb <daniellins.b@gmail.com>
2025-05-26 23:40:40 -0300

    Second commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <h2>
+          Lisboa Advogados !!
+        </h2>
+        <h2>Site </h2>
+      </div>
+      
+    </main>
+  );
+}
